915daaa05e3d6eb7e09247ffd7d254872b5a2401
Victoria Kurzweg <vkurzweg@gmail.com>
2025-08-13 06:06:26 -0700

    Fix: serve build from correct root path for Heroku monorepo

--- namingops/server/src/index.js
+++ namingops/server/src/index.js
@@ -139,12 +139,12 @@ app.get('/test', (req, res) => {
 });
 
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'build')));
+const buildPath = path.resolve(__dirname, '..', 'build');
 
+app.use(express.static(buildPath));
 app.get('*', (req, res) => {
-  // Only serve index.html for non-API routes
   if (!req.path.startsWith('/api')) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(buildPath, 'index.html'));
   }
 });
 
